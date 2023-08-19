3a6992e8d9d4b656f21ba48a08b80f4c80f29830
Leedom <jld1221@163.com>
2023-08-20 00:50:09 +0800

    feat: add fixtures

---
+++ fixtures/vue-h5-template/src/views/error-page/404.vue
@@ -0,0 +1,48 @@
+/** * Created by Leedom on 2022-11-16 01:32:55 */
+
+<template>
+  <div class="no-page">
+    <div class="flex flex-col justify-center items-center">
+      <h1>404</h1>
+      <p>{{ time }}秒后回到首页</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error404Component',
+  components: {},
+  props: {},
+  data() {
+    return {
+      inter: null,
+      time: 5,
+    }
+  },
+  watch: {},
+  created() {},
+  mounted() {
+    this.inter = setInterval(() => {
+      this.time--
+      if (this.time === 0) {
+        this.returnHome()
+      }
+    }, 1000)
+  },
+  unmounted() {
+    clearInterval(this.inter)
+  },
+  methods: {
+    returnHome() {
+      this.$router.replace('/')
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.no-page {
+  padding-top: 40vh;
+}
+</style>
