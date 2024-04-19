6c3c47c069bb24fa1d1b519baf6af32092eebc58
Leedom <jld1221@163.com>
2024-04-19 23:06:54 +0800

    feat(ts): set 1tbs for brace-style

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -3,6 +3,15 @@ const basic = require('@leedomjs/eslint-config-basic')
 module.exports = {
   extends: ['@antfu/eslint-config-ts', '@leedomjs/eslint-config-basic'],
   ignorePatterns: ['*.md'],
+  rules: {
+    '@stylistic/ts/brace-style': [
+      2,
+      '1tbs',
+      {
+        allowSingleLine: true,
+      },
+    ],
+  },
   overrides: [
     ...basic.overrides,
   ],
